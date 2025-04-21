3c99b958cdd1b440121b98daa4e07bebd7314c68
Badawy <mohamed.ali.badawy.pr@gmail.com>
2025-04-21 20:14:53 +0200

    handling a bug in the roomValidation.js.

--- utils/Validations/models/room/roomValidation.js
+++ utils/Validations/models/room/roomValidation.js
@@ -1,13 +1,16 @@
 import Joi from 'joi';
-import joiObjectid from 'joi-objectid';
 
-const JoiObjectId = joiObjectid(Joi);
-
-export default (review) => {
+export default (room) => {
     const schema = Joi.object({
-        rating: Joi.number().min(1).max(5).optional(),
-        comment: Joi.string().min(5).max(500).optional(),
+        name: Joi.string().min(3).max(100).required(),
+        type: Joi.string().valid('meeting', 'Personal', 'shared space', 'cabin').required(),
+        capacity: Joi.number().min(1).max(100).required(),
+        pricePerHour: Joi.number().min(1).required(),
+        availabilityStatus: Joi.string().valid('available', 'unavailable').default('available').required(),
+        isBooked: Joi.boolean().default(false),
+        duration: Joi.number().min(0).optional(),
+        amenities: Joi.array().items(Joi.string()).optional()
     });
 
-    return schema.validate(review);
-};
+    return schema.validate(room);
+}
